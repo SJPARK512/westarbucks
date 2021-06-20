92f7ad7cfb2c2f5015a180a0f1814154ee9689e5
박성준 <seong_jun@jun.psj>
2021-06-21 01:17:49 +0900

    Westarbucks

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'qkzpahshfl1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-&6vig5mkq6hped+zikish%%&9b8@-16jp7or$w&fgo%1n2o%^@'
